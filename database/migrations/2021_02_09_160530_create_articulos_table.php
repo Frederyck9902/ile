c7b12ab7a95bee7206e9c377f7cccd59100799ba
fredery <frederyck9902@gmail.com>
2021-02-09 17:50:11 -0500

    cambio de bd

---
+++ database/migrations/2021_02_09_160530_create_articulos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_capitulo');
+            $table->string("nombre");
+            $table->text("contenido");
+            $table->timestamps();
+            $table->foreign("id_capitulo")
+                  ->references("id")
+                  ->on("capitulos")
+                  ->onDelete("cascade")
+                  ->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.master')
 @section('content')
-<div class="row">        
-    @foreach( $ley as $key => $pelicula )       
-    <div class="col-xs-6 col-sm-4 col-md-3 text-center">                       
-          <h4 style="min-height:45px;margin:5px 0 10px 0">                   
-          {{$pelicula->ley}}      
-           </h4>           
-         </a>        
-       </div>       
-    @endforeach 
+<?php
+  $elementos= array($titulo,$capitulo);
+
+?> 
+<div class="content text-center">              
+        @foreach( $elementos as list($titulo,$capitulo)) 
+        <div>                                        
+          <h4>{{$titulo->nombre}}</h4>
+        </div>                        
+        @endforeach
 </div>
 @stop
