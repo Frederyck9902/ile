6b46734fc7cb0178a3cead86361ac376f044a48c
fredery <frederyck9902@gmail.com>
2021-02-13 14:48:15 -0500

    validacion login

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,10 +12,11 @@
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">               
                     <div class="navbar-nav">                    
                         <a class="nav-link active" href="/">Inicio</a>
-                        <a class="nav-link" href="{{url('/edit')}}">Editar</a>
+                        
                         <a class="nav-link" href="{{url('/reporte')}}">Reporte</a>    
                         <a class="nav-link" href="{{url('/nosotros')}}">Nosotros</a>                           
-                        @if( Auth::check() )                 
+                        @if( Auth::check() )    
+                        <a class="nav-link" href="{{url('/edit')}}">Editar</a>             
                         <form action="{{ url('/logout') }}" method="POST" style="display:inline">
                             {{ csrf_field() }}
                             <button type="submit" class="btn btn-link nav-link" style="display:inline;cursor:pointer">
